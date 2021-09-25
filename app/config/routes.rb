75f0e1440669dfb3d7cf025a7208b423452cda8f
sf-12 <s.fukasawa12@gmail.com>
2021-09-25 22:53:15 +0900

    actions_controllerを追加

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'omniauth_callbacks'
   }
-
+  resources :actions
   resources :maps
   resources :users, only: [:update]
   root to: 'home#index'
