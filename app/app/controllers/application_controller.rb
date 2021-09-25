7a0858056c8ec7af2b6ee33927529ce72bbf55af
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-09-26 01:12:42 +0900

    [add] ログアウト先と、ループバグの中止

--- app/app/controllers/application_controller.rb
+++ app/app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
   def after_sign_in_path_for(resource)
     maps_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
   end
 end
