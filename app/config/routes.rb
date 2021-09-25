f8de852cb4d7eb7ef4756d4a7ce7d5d3d9241e0e
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-09-25 21:13:40 +0900

    [add] updateのルーティング

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: 'omniauth_callbacks'
   }
+
+  resources :users, only: [:update]
   
   # root 'users#index'
   root to: 'home#index'
